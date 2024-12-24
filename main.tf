43a4f1ff8689e2944d1e134fa3501aacf848bbd8
VaniSu <vanisuresh2102@gmail.com>
2024-12-24 12:12:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 # Configure the AWS provider
 provider "aws" {
   region = "us-east-1" # Change to your preferred region
